c924f2c699d8cb85263545275d6968606480de08
Sibi Rajan <sibirajan72@gmail.com>
2025-06-30 14:48:00 +0530

    Time: 29 ms (13.38%), Space: 53.4 MB (71.02%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        HashMap<String,Integer> map = new HashMap<>();
+
+        int k = 10;
+        for (int i = 0; i <s.length() - k+1; i++) {
+            String v = s.substring(i, i + k);
+            map.put(v , map.getOrDefault(v,0)+1);
+        }
+int max =0;
+List<String> list = new ArrayList<>();
+      for(String key : map.keySet()){
+        if(map.get(key)>1){
+            list.add(key);
+        }
+      }
+        return list;
+
+    }
+}
