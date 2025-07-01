88b3ea5ea53ec674820ee061d13004a8bffead97
Sibi Rajan <sibirajan72@gmail.com>
2025-07-01 10:35:29 +0530

    Time: 0 ms (100%), Space: 42.1 MB (78.51%) - LeetHub

--- 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.java
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.java
@@ -1,12 +1,12 @@
 class Solution {
     public int possibleStringCount(String s) {
-        int count =0;
+        int count =1;
         for(int i = s.length()-1;i>=1;i--){
             if(s.charAt(i)==s.charAt(i-1)){
                 count++;
             }
         }
-         return count+1;
+         return count;
  
         
         
