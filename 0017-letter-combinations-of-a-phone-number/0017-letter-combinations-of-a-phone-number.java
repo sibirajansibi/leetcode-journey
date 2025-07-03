873a351519d067fa78d9857a3bb2d77640acf544
Sibi Rajan <sibirajan72@gmail.com>
2025-07-03 08:19:02 +0530

    Time: 0 ms (100%), Space: 42.4 MB (38.35%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,6 +1,6 @@
 class Solution {
 
-     private static final Map<Character, String> phoneMap = new HashMap<>();
+    private static final Map<Character, String> phoneMap = new HashMap<>();
 
     static {
         phoneMap.put('2', "abc");
@@ -13,37 +13,27 @@ class Solution {
         phoneMap.put('9', "wxyz");
     }
 
-
     public List<String> letterCombinations(String s) {
-        if(s.length() == 0){
+        if (s.length() == 0) {
             return new ArrayList<>();
         }
         List<String> result = new ArrayList<>();
-        solver(0 , s , result , new StringBuilder());
+        solver(0, s, result, new StringBuilder());
         return result;
     }
 
-    public static void solver(int  start ,String s , List<String> result , StringBuilder sb){
+    public static void solver(int start, String s, List<String> result, StringBuilder sb) {
 
-     if(start == s.length()){
-       result.add(sb.toString());
-       return;
-      }
-      String possible = phoneMap.get(s.charAt(start));
+        if (start == s.length()) {
+            result.add(sb.toString());
+            return;
+        }
+        String possible = phoneMap.get(s.charAt(start));
 
-      for(char c : possible.toCharArray()){
-        sb.append(c);
-        solver(start +1 ,s ,result , sb);
-        sb.deleteCharAt(sb.length()-1);
-      }
+        for (char c : possible.toCharArray()) {
+            sb.append(c);
+            solver(start + 1, s, result, sb);
+            sb.deleteCharAt(sb.length() - 1);
+        }
     }
-    
-
-
-
-
-
-
-
-
 }
