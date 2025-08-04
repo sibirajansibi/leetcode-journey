8d441e041f68a3808e8e5770ca8e2e6b9a9d5971
Sibi Rajan <sibirajan72@gmail.com>
2025-08-04 07:08:39 +0530

    Time: 5 ms (21.96%), Space: 45.4 MB (80.76%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -2,28 +2,27 @@ class Solution {
     public boolean validPalindrome(String s) {
         int i = 0;
         int j = s.length() - 1;
-        if(isPalin(s , i , j)){
+        if (isPalin(s, i, j)) {
             return true;
         }
         while (i < j) {
             if (s.charAt(i) != s.charAt(j)) {
                 return isPalin(s, i + 1, j) || isPalin(s, i, j - 1);
             }
-             i++;
-                j--;
+            i++;
+            j--;
         }
         return false;
     }
 
-    public static boolean isPalin(String s , int i , int j  ){
+    public static boolean isPalin(String s, int i, int j) {
 
-        
-        while(i<j){
-        if(s.charAt(i)!=s.charAt(j)){
-            return false;
-        }
-        i++;
-        j--;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
         }
         return true;
     }
