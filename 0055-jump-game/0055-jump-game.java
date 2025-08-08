f8edff35840a53ccfa11ffeaa5728e073d23d5c0
Sibi Rajan <sibirajan72@gmail.com>
2025-08-08 22:00:47 +0530

    Time: 2 ms (83.24%), Space: 46 MB (31.22%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,16 +1,17 @@
-
-
-
 class Solution {
     public boolean canJump(int[] nums) {
-        int goal = nums.length - 1;
 
-        for (int i = nums.length - 2; i >= 0; i--) {
-            if (i + nums[i] >= goal) {
-                goal = i;
-            }
-        }
 
-        return goal == 0;        
+int max  =0;
+for(int i =0;i<nums.length;i++){ // i =0; i = 1
+    if(i> max){    
+        return false; 
+    }
+    //           (0 , 0+2)( 1, 3+1)
+    max = Math.max(max , nums[i]+i);
+}
+
+return true;
+        
     }
 }
