20fcd6ec1a3e70700010ab56c5c9a1d0f743a5c7
Sibi Rajan <sibirajan72@gmail.com>
2025-08-30 14:41:43 +0530

    Time: 11 ms (95.28%), Space: 64 MB (5.14%) - LeetHub

--- 3979-partition-array-into-k-distinct-groups/3979-partition-array-into-k-distinct-groups.java
+++ 3979-partition-array-into-k-distinct-groups/3979-partition-array-into-k-distinct-groups.java
@@ -1,20 +1,26 @@
+import java.util.*;
+
 class Solution {
     public boolean partitionArray(int[] nums, int k) {
+        int n = nums.length;
+        
+        // If array length is not divisible by k, partition impossible
+        if (n % k != 0) return false;
+        
+        int size = n / k;
+        
+        // Sort the numbers to group them easily
+        Arrays.sort(nums);
 
-        if (nums.length % k != 0) {
-            return false;
-        }
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-        int size = nums.length/k;
-
-        for(int num : map.values()){
-            if(num>size){
-                return false;
+        int count = 1; // count frequency of current number
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1]) {
+                count++;
+                if (count > size) return false; // same number too frequent
+            } else {
+                count = 1; // reset when number changes
             }
         }
-return true;
+        return true;
     }
 }
