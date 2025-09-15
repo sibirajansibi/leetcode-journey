1879d369f2ead8836b4495faac7589d79d2abff6
Sibi Rajan <sibirajan72@gmail.com>
2025-09-15 16:32:12 +0530

    Time: 17 ms (44.64%), Space: 46.5 MB (28.09%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,30 +1,18 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-               int[] ans = new int[nums.length*2];
-                int n = nums.length;
-            for(int i =0;i<ans.length;i++){
-                ans[i] = nums[i%n];
+        Stack<Integer> stack = new Stack<>();
+        int[] ans = new int[nums.length];
+        for (int i = 2 * nums.length - 1; i >= 0; i--) { //
+            while (!stack.isEmpty() && stack.peek() <= nums[i % nums.length]) {
+                stack.pop();
             }
-        for(int i = 0;i<nums.length;i++){
-            int get = nums[i];
-            ans[i] = isCheck(get,ans, i);
-        }
-        int k =0;
-        
-        for(int i =0;i<n;i++){
-             nums[i] = ans[i];    
-        }
- return nums;
-        }
-    
-
-    public static int isCheck(int n , int[] ans, int start){
-
-          for(int i= start;i<ans.length;i++){
-            if(n<ans[i]){
-                return ans[i];
+            if (i < nums.length) {
+                ans[i] = stack.isEmpty() ? -1 : stack.peek();
             }
-          }
-          return -1;
+
+            stack.push(nums[i % nums.length]);
+        }
+        return ans;
+
     }
 }
