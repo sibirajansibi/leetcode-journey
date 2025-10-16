6dd36f43f4f73bac066a1c26ae9824bb9b5539e6
Sibi Rajan <sibirajan72@gmail.com>
2025-10-16 19:49:22 +0530

    Time: 71 ms (97.17%), Space: 45.7 MB (19.37%) - LeetHub

--- 4055-longest-balanced-substring-i/4055-longest-balanced-substring-i.java
+++ 4055-longest-balanced-substring-i/4055-longest-balanced-substring-i.java
@@ -2,25 +2,29 @@ class Solution {
     public int longestBalanced(String s) {
         int n = s.length();
         int maxLen = 0;
+        // Try every possible substring starting at i
         for (int i = 0; i < n; i++) {
             int[] freq = new int[26];
+            int distinct = 0;      // Number of distinct characters in the window
             for (int j = i; j < n; j++) {
-                freq[s.charAt(j) - 'a']++;
-                if (isBalanced(freq)) {
-                    maxLen = Math.max(maxLen, j - i + 1);
+                int idx = s.charAt(j) - 'a';
+                freq[idx]++;
+                if (freq[idx] == 1) distinct++;
+
+                if ((j - i + 1) % distinct == 0) { 
+                    int val = freq[s.charAt(i) - 'a'];
+                    boolean balanced = true;
+                    for (int k = 0; k < 26; k++) {
+                        if (freq[k] == 0) continue;
+                        if (freq[k] != val) {
+                            balanced = false;
+                            break;
+                        }
+                    }
+                    if (balanced) maxLen = Math.max(maxLen, j - i + 1);
                 }
             }
         }
         return maxLen;
-    }
-    
-    private boolean isBalanced(int[] freq) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int num : freq){
-            if( num ==0) continue;
-            set.add(num);
-        }
-        
-        return set.size()==1;
     }
 }
