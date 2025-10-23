7cff8ad78b393e6133457911f5b38f6ec40d9cb0
Sibi Rajan <sibirajan72@gmail.com>
2025-10-23 14:12:29 +0530

    Time: 3 ms (14.34%), Space: 41.9 MB (19.67%) - LeetHub

---
+++ 1970-sorting-the-sentence/1970-sorting-the-sentence.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String sortSentence(String s) {
+        TreeMap<Integer,String> map = new TreeMap<>();
+StringBuilder sb = new StringBuilder();
+        String[] srr = s.split("\\s+");
+     
+        for(int i =0;i<srr.length;i++){
+            String v = srr[i];
+            int key = Integer.valueOf(v.charAt(v.length()-1));
+               v = v.substring(0,v.length()-1);
+            map.put(key,v);
+        }
+        System.out.print(map);
+
+        for(String  ss : map.values()){
+sb.append(ss+" ");
+        }
+        return sb.toString().trim();
+    }
+}
