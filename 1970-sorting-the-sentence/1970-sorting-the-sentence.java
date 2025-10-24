1e2494d8c924a20e8473fff3deb2abadaee92291
Sibi Rajan <sibirajan72@gmail.com>
2025-10-24 11:10:04 +0530

    Time: 2 ms (18.35%), Space: 41.9 MB (19.49%) - LeetHub

--- 1970-sorting-the-sentence/1970-sorting-the-sentence.java
+++ 1970-sorting-the-sentence/1970-sorting-the-sentence.java
@@ -1,17 +1,15 @@
 class Solution {
     public String sortSentence(String s) {
         String[] arr = s.split("\\s+");
-        
+        StringBuilder sb = new StringBuilder();
         Arrays.sort(arr , (a, b)-> a.charAt(a.length()-1) - b.charAt(b.length()-1));
         for(int i=0;i<arr.length;i++){
             String v  = arr[i];
             arr[i] = v.substring(0, v.length()-1);
+            sb.append(arr[i]+" ");
         }
-        StringBuilder sb = new StringBuilder();
-        for(String  sv : arr){
-            sb.append(sv+" ");
-        }
-
+        
+       
         return sb.toString().trim();
     }
 }
