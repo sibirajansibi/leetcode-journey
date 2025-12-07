5b06f52ca54014514c08627ce67ef1171ca123d6
Sibi Rajan <sibirajan72@gmail.com>
2025-12-07 22:15:53 +0530

    Time: 3 ms (99.13%), Space: 47.9 MB (7.86%) - LeetHub

---
+++ 1616-split-two-strings-to-make-palindrome/1616-split-two-strings-to-make-palindrome.java
@@ -0,0 +1,33 @@
+class Solution {
+   public static boolean checkPalindromeFormation(String a, String b) {
+        return check(a, b) || check(b, a);
+    }
+
+    public static boolean check(String a, String b) {
+        int i = 0;
+        int j = b.length()-1;
+        while (i < j) {
+            if(a.charAt(i) == b.charAt(j)){
+                i++;
+                j--;
+            }
+            else{
+                break;
+            }
+        }
+        return isPalindrome(a, i, j) || isPalindrome(b, i, j);
+
+    }
+
+    public static boolean isPalindrome(String s, int i , int j ){
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+            
+        }
+        return true;
+    }
+}
