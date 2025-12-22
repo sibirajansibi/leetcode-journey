4c08ebc83aa6d57786940e33021e9f8d209213ff
Sibi Rajan <sibirajan72@gmail.com>
2025-12-22 20:14:12 +0530

    Time: 1 ms (71.65%), Space: 42.8 MB (54.71%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String interpret(String command) {
+        Stack<Character> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+     command = command.replace("()","o").replace("(al)","al");
+     return command;
+    }
+}
