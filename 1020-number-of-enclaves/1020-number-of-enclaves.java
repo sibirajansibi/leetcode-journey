12ee568ad317966da97da7e927813b47c3e6deac
Sibi Rajan <sibirajan72@gmail.com>
2026-02-26 14:45:08 +0530

    Time: 7 ms (84.49%), Space: 61.2 MB (27.89%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,40 +1,39 @@
 class Solution {
- 
     public int numEnclaves(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-      
-       int count = 0;
-       for(int i = 0;i<n;i++){
-            BoundDfs(i , 0, grid);
-             BoundDfs(i , m-1, grid);
-       }
-       for(int i = 0;i<m;i++){
-            BoundDfs(0, i, grid);
-             BoundDfs(0 , n-1, grid);
-       }
 
-     // Java example
-    
-        for(int i =0;i < n;i++){
-            for(int j =0;j < m;j++){
-               if( grid[i][j] == 1){
-                   count++;
-               }
+        // Step 1: Remove boundary-connected land
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if((i == 0 || j == 0 || i == n-1 || j == m-1) && grid[i][j] == 1) {
+                    dfs(i, j, grid);
+                }
             }
         }
 
+        // Step 2: Count remaining land
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 1) {
+                    count++;
+                }
+            }
+        }
 
         return count;
     }
-    public void BoundDfs(int i , int j ,int[][] grid){
-        if(i < 0 || j < 0 || i>= grid.length || j >= grid[0].length || grid[i][j] == 0){
+
+    private void dfs(int i, int j, int[][] grid) {
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0)
             return;
-        }
+
         grid[i][j] = 0;
-        BoundDfs(i+1, j, grid);
-        BoundDfs(i ,  j+1, grid);
-        BoundDfs(i-1, j, grid);
-        BoundDfs(i,   j-1, grid);
+
+        dfs(i+1, j, grid);
+        dfs(i-1, j, grid);
+        dfs(i, j+1, grid);
+        dfs(i, j-1, grid);
     }
 }
